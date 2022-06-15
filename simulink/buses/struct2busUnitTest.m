89c3ab0040a7be0b7f5066672e4b8cd6484cba02
Yannic Beyer <y.beyer@tu-bs.de>
2022-06-15 15:17:52 +0200

    Struct2bus: Move struct2slbus to struct2bus
    After the previous updates, probably only one
    of these function is needed. Struct2slbus can
    probably be deleted in the future.
    
    Ref. #2

--- simulink/buses/struct2busUnitTest.m
+++ simulink/buses/struct2busUnitTest.m
@@ -1,4 +1,4 @@
-% struct2slbusUnitTest implements struct2bus test functions in m-Utils
+% struct2busUnitTest implements struct2bus test functions in m-Utils
 %
 %   These functions define test cases with well known results.
 %   This unit test should be run every time the file has been modified,
@@ -7,7 +7,7 @@
 %   script can be used to find unintended errors.
 %
 % Example:
-%   rt = table(runtests('struct2slbusUnitTest'))
+%   rt = table(runtests('struct2busUnitTest'))
 %
 % Literature: 
 %   [1] https://blogs.mathworks.com/loren/2013/10/15/function-is-as-functiontests/
@@ -23,7 +23,7 @@
 % *************************************************************************
 
 % Call all subfunction tests in this file
-function tests = struct2slbusUnitTest
+function tests = struct2busUnitTest
     tests = functiontests(localfunctions);
 end
 
@@ -51,8 +51,8 @@ test_struct.g.c = sub_struct3;
 
 assignin('base','test_struct',test_struct);
 
-struct2slbus(test_struct,'test_struct')
+struct2bus(test_struct,'test_struct')
 
-sim('struct2slbus_sim_test');
+sim('struct2bus_sim_test');
 
 end
--- simulink/buses/struct2slbus.m
+++ simulink/buses/struct2slbus.m
@@ -1,5 +1,7 @@
 function id = struct2slbus( s, varargin )
 % struct2slbus create simulink bus object(s) from struct
+%   THIS FUNCTION WILL PROBABLY BE REPLACED BY STRUCT2BUS IN THE FUTURE.
+%   PLEASE USE STRUCT2BUS INSTEAD OF THIS FUNCTION.   
 % 
 % Syntax:
 %   id = struct2slbus( s )
@@ -33,94 +35,6 @@ else
     bus_name = varargin{1};
 end
 
-% Obtain the fieldnames of the structure
-sfields = fieldnames(s);
-
-if( length(s) > 1 )
-    s = s(1);
-end
-
-elems = repmat( Simulink.BusElement, length(sfields), 1 );
-
-% Loop through the structure
-for i = 1:length(sfields)
-    
-    sfields_diff = sfields{i};
-    
-    % fill BusElement for each field
-    if isequal( class(s.(sfields{i})), 'struct' )
-        % Here is struct inside a struct identified. And only existing types
-        % could be added as type in this script. So we have to create a bus
-        % for this struct and add them to this element.
-        % Create new bus
-        id = struct2slbus( s.(sfields{i}), sfields_diff );
-        if isempty(id)
-            final_bus_name = [sfields_diff, 'Bus'];
-        else
-            final_bus_name = [sfields_diff, 'Bus',num2str(id)];
-        end
-        elems(i).DataType = ['Bus: ', final_bus_name];
-    else
-        
-        elems(i) = Simulink.BusElement;
-        % avoid problems with generated C/C++ code
-        if strcmp(elems(i).DataType, 'logical')
-            elems(i).DataType = 'boolean';
-        else
-            elems(i).DataType = class(s.(sfields{i}));
-        end
-    end
-    elems(i).Name = sfields{i};
-    elems(i).Dimensions = size(s.(sfields{i}));
-    elems(i).SampleTime = -1;
-    elems(i).Complexity = 'real';
-    elems(i).SamplingMode = 'Sample based';
-end
-
-% Create main fields of Bus Object and generate Bus Object in the base
-% workspace.
-BusObject = Simulink.Bus;
-BusObject.HeaderFile = '';
-BusObject.Description = sprintf('');
-BusObject.Elements = elems;
-full_bus_name = [ bus_name, 'Bus' ];
-id = assignInBaseNoOverwrite( BusObject, full_bus_name, [] );
-end
-
-function id = assignInBaseNoOverwrite( var, var_name, id )
-if evalin('base',['exist(''',var_name,''')'])
-    base_var = evalin('base',var_name);
-    if isequal( base_var, var )
-        % do nothing if the same bus exists with the same name
-        return;
-    else
-        % increment appended name number (id) and save the new id
-        [var_name,id] = incrementName( var_name );
-        id = assignInBaseNoOverwrite( var, var_name, id );
-    end
-else
-    % assign bus to base workspace if it does not exist
-    assignin('base', var_name, var);
-end
-
-    function [name,id] = incrementName( name )
-        % detect current appended bus name number (id) and increment it
-        i = 0;
-        while true
-            current_char = str2double(name(end-i));
-            if isnan( current_char )
-                i = i - 1;
-                break;
-            else
-                i = i + 1;
-            end
-        end
-        id = str2double(name(end-i:end));
-        if isnan(id)
-            id = 0;
-        end
-        id = id + 1;
-        name = [name(1:end-i-1),num2str(id)];
-    end
+id = struct2bus( s, bus_name );
 
 end
