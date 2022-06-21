96d8610009b1cb4a83859f39f4375893dde3ec03
Yannic Beyer <y.beyer@tu-bs.de>
2022-06-21 09:35:26 +0200

    Add faster cross function alternative

---
+++ operations/crossFast.m
@@ -0,0 +1,31 @@
+function c = crossFast(a,b)
+% crossFast faster implementation of cross function
+%   This function only works for 3xN array inputs but is much faster
+%   compared to cross.
+% 
+% Syntax:
+%   c = crossFast( a, b )
+% 
+% Inputs:
+%   a               first vectors (3xN array)
+%   b               second vectors (3xN array)
+% 
+% Outputs:
+%   c               product of inputs (3xN array)
+% 
+% See also:
+%   cross
+
+% Disclamer:
+%   SPDX-License-Identifier: GPL-2.0-only
+% 
+%   Copyright (C) 2022 Yannic Beyer
+%   Copyright (C) 2022 TU Braunschweig, Institute of Flight Guidance
+% *************************************************************************
+    
+% Calculate cross product
+c(3,:) = a(1,:).*b(2,:)-a(2,:).*b(1,:);
+c(1,:) = a(2,:).*b(3,:)-a(3,:).*b(2,:);
+c(2,:) = a(3,:).*b(1,:)-a(1,:).*b(3,:);
+    
+end
