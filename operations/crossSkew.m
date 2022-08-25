2bed5b1478abb4bd21659355bb824b50af75cf6c
Yannic Beyer <y.beyer@tu-bs.de>
2022-08-25 19:49:33 +0200

    Add skew matrix for cross products function

---
+++ operations/crossSkew.m
@@ -0,0 +1,39 @@
+function a_x = crossSkew( a )
+% crossSkew 3x3 skew-symmetric matrix to represent cross product as matrix
+%   multiplication
+%   The cross product matrix can replace the cross product
+%       c = cross(a,b)
+%   by a matrix multiplication (see [1], "Cross product" section)
+%       c = a_x * b
+% 
+% Syntax:
+%   a_x = crossSkew(a) 
+% 
+% Inputs:
+%   a                   3d vector (3x1 array)
+% 
+% Outputs:
+%   a_x                 3x3 skew-symmetric matrix (3x3 array)
+% 
+% Example:
+%   a = rand(3,1);
+%   b = rand(3,1);
+%   c1 = cross(a,b);
+%   a_x = crossSkew(a);
+%   c2 = a_x*b;
+%   is_diff_small = norm(c1-c2,2) < 1e-15
+% 
+% Literature:
+%   [1] https://en.wikipedia.org/wiki/Skew-symmetric_matrix
+
+% Disclamer:
+%   SPDX-License-Identifier: GPL-2.0-only
+% *************************************************************************
+
+a_x = [ ...
+        0,      -a(3),	a(2); ...
+        a(3),   0,      -a(1); ...
+        -a(2),  a(1),   0 ...
+    ];
+
+end
